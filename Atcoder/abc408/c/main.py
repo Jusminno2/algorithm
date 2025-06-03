9e2f39cb614b2263bf5e25e92f5a4c3bc8085d3e
Jusminno2 <g.urakawa@gmail.com>
2025-06-03 14:03:57 +0900

    いもす法

--- Atcoder/abc408/c/main.py
+++ Atcoder/abc408/c/main.py
@@ -1,13 +1,13 @@
-N, M = map(int, input().split())
+N ,M = map(int, input().split())
 imos = [0] * (N+1)
-for _ in range(M):
+for i in range(M):
     l, r = map(int, input().split())
     l = l-1
     imos[l] += 1
     imos[r] -= 1
+for i in range(1, N+1):
+    imos[i] += imos[i-1]
 ans = 1e9
-for i in range(1, N):
-    imos[i] = imos[i-1] + imos[i]
+for i in range(N):
     ans = min(ans, imos[i])
-
 print(ans)
