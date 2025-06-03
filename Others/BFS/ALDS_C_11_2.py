2b062e5c16e1755615452a7db02c64526df592b9
Jusminno2 <g.urakawa@gmail.com>
2025-06-04 08:15:52 +0900

    BFS

---
+++ Others/BFS/ALDS_C_11_2.py
@@ -0,0 +1,38 @@
+from collections import deque
+from typing import List
+
+
+def bfs(G: List[int], n: int) -> List[int]:
+    seen = deque()
+    seen.append(1)
+    dist = [-1] * (n+1)
+    dist[1] = 0
+
+    while seen:
+        v = seen.popleft()
+        for next_v in G[v]:
+            if dist[next_v] == -1:
+                dist[next_v] = dist[v] + 1
+                seen.append(next_v)
+
+    return dist
+
+
+def main():
+    n = int(input())
+    G = [[] for _ in range(n + 1)]
+
+    for _ in range(n):
+        arr = list(map(int, input().split()))
+        u = arr[0]
+        k = arr[1]
+        G[u] = arr[2:]
+
+    dist = bfs(G, n)
+
+    for i in range(1, n + 1):
+        print(f"{i} {dist[i]}")
+
+
+if __name__ == "__main__":
+    main()
