8bbfd6e17eddd62fdfbd8c9a87361de45af2cfcb
Jusminno2 <g.urakawa@gmail.com>
2025-06-09 09:20:47 +0900

    abc409

--- Atcoder/abc409/b/main.py
+++ Atcoder/abc409/b/main.py
@@ -1,1 +1,13 @@
+N = int(input())
+A = list(map(int, input().split()))
 
+ans = 0
+for i in range(N+1):
+    count = 0
+    for a in A:
+        if a >= i:
+            count += 1
+    if count >= i:
+        ans = max(ans, i)
+
+print(ans)
