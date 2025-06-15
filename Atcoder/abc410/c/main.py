afdda7bd2775dd05efeefda7f57b4cd3ed74de72
Jusminno2 <g.urakawa@gmail.com>
2025-06-15 09:41:32 +0900

    abc410C問題はoffset

--- Atcoder/abc410/c/main.py
+++ Atcoder/abc410/c/main.py
@@ -3,26 +3,27 @@ from collections import deque
 N, Q = map(int, input().split())
 A = [int(i) for i in range(1, N + 1)]
 
-def rotate(a,n):
-    return a[n:] + a[:n]
+offset = 0
 
 for _ in range(Q):
     q = list(map(int, input().split()))
 
     # type1
     if q[0] == 1:
-        p, x  = q[1], q[2]
-        A[p-1] = x
+        p, x  = q[1]-1, q[2]
+        A[(p + offset) % N] = x
 
     # type2
     elif q[0] == 2:
-        p = q[1]
-        print(A[p-1])
+        p = q[1] -1
+        print(A[(p + offset) % N])
 
     # type3
     elif q[0] == 3:
-        k = N - q[1] % N
-        A = rotate(A, N-k)
+        k = q[1]
+        offset = (offset + k) % N
+
+
 
 
 
