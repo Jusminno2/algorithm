489b36719d7d9536524293e09f3f017f99c09998
Jusminno2 <g.urakawa@gmail.com>
2025-06-30 09:41:55 +0900

    b問題無問題

--- Atcoder/abc412/b/main.py
+++ Atcoder/abc412/b/main.py
@@ -1,1 +1,12 @@
+S = input()
+T = input()
+
+for i in range(len(S)):
+    if i > 0:
+        if S[i].isupper():
+            if S[i - 1] not in T:
+                print("No")
+                exit()
+
+print("Yes")
 
