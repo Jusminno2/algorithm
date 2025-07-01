174a7999fc3fe89104a11a24efd2eebb4c957e24
Jusminno2 <g.urakawa@gmail.com>
2025-07-02 08:50:26 +0900

    abc207-b 条件を満たさない末端のケースについて深く考えることが大切

--- Atcoder/abc207/b/main.py
+++ Atcoder/abc207/b/main.py
@@ -1,1 +1,24 @@
+a, b, c, d = map(int, input().split())
+count = 0
+red = a
+blue = 0
+
+if b > c * d:
+    print(-1)
+    exit()
+
+if a == 0:
+    print(0)
+    exit()
+
+if red < blue * d:
+    print(red, blue * d)
+
+while red >= blue * d:
+    count += 1
+    red = red + b
+    blue = blue + c
+
+print(count)
+
 
