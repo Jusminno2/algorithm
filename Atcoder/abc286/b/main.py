348da2d0f174b35a3bb44a4a26e8ebab2b8456e8
Jusminno2 <g.urakawa@gmail.com>
2025-07-14 21:46:13 +0900

    abc286 b

--- Atcoder/abc286/b/main.py
+++ Atcoder/abc286/b/main.py
@@ -1,1 +1,15 @@
+N = int(input())
+S = input()
+ans = ""
+
+for i in range(N):
+    if i == N-1:
+        ans += S[i]
+    elif S[i] == "n" and S[i+1] == "a":
+        ans += S[i]
+        ans += "y"
+    else:
+        ans += S[i]
+
+print(ans)
 
