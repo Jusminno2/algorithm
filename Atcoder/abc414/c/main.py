33cecbedffcf17a98bd9ee2abb30e8be39943aea
Jusminno2 <g.urakawa@gmail.com>
2025-07-15 22:54:20 +0900

    abc414 C nの前ループはできないよ

--- Atcoder/abc414/c/main.py
+++ Atcoder/abc414/c/main.py
@@ -1,1 +1,37 @@
+"""
+point:
+- 回文の判定
+- N進法をどうやって導くか？
+"""
+def check_palindrome(string):
+    if string == string[::-1]:
+        return True
+    else:
+        return False
 
+def base_n(num_10,n):
+    str_n = ''
+    while num_10:
+        if num_10 % n >= 10:
+            return -1
+        str_n += str(num_10%n)
+        num_10 //= n
+    return int(str_n[::-1])
+
+
+def main():
+    A = int(input())
+    N = int(input())
+    ans = 0
+
+    for i in range(N):
+        if check_palindrome(str(i)) and check_palindrome(str(base_n(i, A))):
+            ans += i
+
+    print(ans)
+
+
+
+
+if __name__ == '__main__':
+    main()
