ce6e65974d521d3992795e33fbc2cb66e87a6cdd
Jusminno2 <g.urakawa@gmail.com>
2025-07-16 09:31:18 +0900

    abc100c

--- Atcoder/abc100/c/main.py
+++ Atcoder/abc100/c/main.py
@@ -1,1 +1,21 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+ans = 0
+
+"""
+example:
+3
+5 2 4
+"""
+
+for i in range(N):
+    count = 0
+    if A[i] % 2 == 0:
+        while A[i] % 2 == 0:
+            A[i] //= 2
+            count += 1
+        ans += count
+
+print(ans)
 
