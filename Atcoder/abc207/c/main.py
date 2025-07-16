6c5c5e557b906cf4a27d3e3e55267560a1eb7849
Jusminno2 <g.urakawa@gmail.com>
2025-07-16 10:16:56 +0900

    abc207c 第2稿

--- Atcoder/abc207/c/main.py
+++ Atcoder/abc207/c/main.py
@@ -1,20 +1,21 @@
 N = int(input())
-t = [-1 for _ in range(N)]
-l = [-1 for _ in range(N)]
-r = [-1 for _ in range(N)]
+l = [0] * N
+r = [0] * N
 for i in range(N):
-    t[i], l[i], r[i] = map(int, input().split())
+    t, l[i], r[i] = map(int, input().split())
+    if t == 2:
+        r[i] -= 0.5
+    elif t == 3:
+        l[i] += 0.5
+    elif t == 4:
+        l[i] += 0.5
+        r[i] -= 0.5
 
 ans = 0
 
 for i in range(N):
-    for j in range(i + 1, N):
-        if t[i] == 1:
-            pass
-        elif t[i] == 2:
-            pass
-        elif t[i] == 3:
-            pass
-        elif t[i] == 4:
-            pass
+    for j in range(i+1, N):
+        if max(l[i], l[j]) <= min(r[i], r[j]):
+            ans += 1
 
+print(ans)
