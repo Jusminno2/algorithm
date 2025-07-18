4893139279d5a4c38efcd73ce447204b4298340b
Jusminno2 <g.urakawa@gmail.com>
2025-07-18 11:19:00 +0900

    abc006c 一旦全探索 10^2

--- Atcoder/abc006/c/main.py
+++ Atcoder/abc006/c/main.py
@@ -1,1 +1,16 @@
+N, M = map(int, input().split())
 
+adult = 2
+elderly = 3
+baby = 4
+
+for ad in range(N+1):
+    for el in range(N - ad + 1):
+        ba = N - ad - el
+        legs = adult * ad + elderly * el + baby * ba
+
+        if legs == M:
+            print(ad, el, ba)
+            exit()
+
+print(-1, -1, -1)
