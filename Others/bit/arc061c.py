ce91d63a0a746d394c9880283901ea04d26ad26a
Jusminno2 <g.urakawa@gmail.com>
2025-07-22 09:36:25 +0900

    arc061 ＋を挿入するべき場所を決める->挿入してjoin->splitしてsum

--- Others/bit/arc061c.py
+++ Others/bit/arc061c.py
@@ -1,21 +1,32 @@
 S = input()
-S_list = [s for s in S]
 
 length = len(S) - 1
 ans = list()
+sum = 0
 
 for i in range(1 << length):
     id_list = list()
+    S_list = [s for s in S]
     gap = 0
     for j in range(length):
         if i & (1 << j):
             id_list.append(j)
+
     for id in id_list:
         S_list.insert(id+gap+1, "+")
         gap += 1
-    print(S_list)
 
-    "+でsplitしたらええか"
+    if not '+' in S_list:
+        sum += int(''.join(S_list))
+    else:
+        culc = ''.join(S_list)
+        culc_list = culc.split('+')
+        for i in culc_list:
+            sum += int(i)
+
+print(sum)
+
+
 
 
 
