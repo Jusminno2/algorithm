41ed374c9d5989d575488bd00790ebb55636cd86
Jusminno2 <g.urakawa@gmail.com>
2025-07-23 18:24:46 +0900

    abc104

--- Atcoder/abc104/b/main.py
+++ Atcoder/abc104/b/main.py
@@ -1,1 +1,18 @@
+S = input()
+count = 0
+
+for i, s in enumerate(S):
+    if i == 0 and s == "A":
+        if 2 <= i <= len(S) and s == "C":
+            count += 1
+
+    else:
+        print("WA")
+        exit()
+
+if count == 1:
+    print("AC")
+else:
+    print("WA")
+
 
