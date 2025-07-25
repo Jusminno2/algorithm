8194030183128b4e35b2626335aeb2dea7e883be
Jusminno2 <g.urakawa@gmail.com>
2025-07-25 09:09:19 +0900

    abc106ab 約数の数

--- Atcoder/abc106/b/main.py
+++ Atcoder/abc106/b/main.py
@@ -4,9 +4,9 @@ ans = list()
 
 for i in range(1, N+1):
     divisors = list()
-    if N % i != 0:
+    if i % 2 != 0:
         for j in range(1, i+1):
-            if N % j == 0:
+            if i % j == 0:
                 divisors.append(j)
         if len(divisors) == 8:
             ans.append(i)
