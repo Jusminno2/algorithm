5f955bb2a19626c030becc0f4b6708ad4f4e032c
Jusminno2 <g.urakawa@gmail.com>
2025-07-27 08:26:26 +0900

    abc416C dfsを使って全範囲探索する

--- Atcoder/abc416/c/main.py
+++ Atcoder/abc416/c/main.py
@@ -1,1 +1,16 @@
+N,K,X=map(int,input().split())
+S=[input() for _ in range(N)]
 
+ans=[]
+
+def dfs(crr, count):
+  # count 個の文字列を結合して crr になった状態
+  if count==K:
+    ans.append(crr)
+    return
+  for s in S:
+    dfs(crr+s, count+1)
+
+dfs("", 0)
+ans.sort()
+print(ans[X-1])
