c86a29fb0b93655f7e4b2f3822e339cdfa545779
Jusminno2 <g.urakawa@gmail.com>
2025-07-28 23:52:43 +0900

    abc008

---
+++ Atcoder/abc008/b/main.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+N = int(input())
+d = defaultdict(int)
+for _ in range(N):
+    k = input()
+    d[k] += 1
+score_sorted = sorted(d.items(), key=lambda x:x[1])
+print(score_sorted)
+d = dict(score_sorted)
+
+for k, v in d.items():
+    print(k)
+    exit()
+
