0d176e47987efd0394bdb6f6bee6f0076073dc51
Jusminno2 <g.urakawa@gmail.com>
2025-07-28 23:54:11 +0900

    abc008 dictの復習

--- Atcoder/abc008/b/main.py
+++ Atcoder/abc008/b/main.py
@@ -5,8 +5,8 @@ d = defaultdict(int)
 for _ in range(N):
     k = input()
     d[k] += 1
-score_sorted = sorted(d.items(), key=lambda x:x[1])
-print(score_sorted)
+score_sorted = sorted(d.items(), key=lambda x:x[1], reverse=True)
+
 d = dict(score_sorted)
 
 for k, v in d.items():
