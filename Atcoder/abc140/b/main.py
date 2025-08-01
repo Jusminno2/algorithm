001e640290f342a7e826fabffd639be72d1ecbec
Jusminno2 <g.urakawa@gmail.com>
2025-08-01 20:54:09 +0900

    abc140b 肩慣らし

--- Atcoder/abc140/b/main.py
+++ Atcoder/abc140/b/main.py
@@ -1,1 +1,16 @@
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = list(map(int, input().split()))
 
+res = 0
+
+for i in range(N):
+    if i < N-1:
+        if A[i+1] - A[i] == 1:
+            res += C[A[i]-1]
+
+    res += B[A[i]-1]
+
+
+print(res)
