68ee6bd7efe0b5f944195bec34ba9aa50045da8c
Jusminno2 <g.urakawa@gmail.com>
2025-08-02 23:16:18 +0900

    abc417abc

---
+++ Atcoder/abc417/c/main.py
@@ -0,0 +1,11 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+count = 0
+
+for i in range(N):
+    for j in range(i+1, N):
+        if j - i == A[i-1] + A[j-1]:
+            count += 1
+
+print(count)
