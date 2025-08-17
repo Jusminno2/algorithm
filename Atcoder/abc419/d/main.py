4cc8b0dc97293dcf6b572499b88e0db161490143
Jusminno2 <g.urakawa@gmail.com>
2025-08-17 09:56:49 +0900

    abc419d 毎回スワップするのではなく、ぐう期に着目することで計算量を減らせる

--- Atcoder/abc419/d/main.py
+++ Atcoder/abc419/d/main.py
@@ -1,22 +1,19 @@
 N, M = map(int, input().split())
-S = list(input())
-T = list(input())
+s = input()
+t = input()
 
+cum = [0] * (N+1)
 
+# いもす法 - Point 1 => 範囲のスタート+1, 範囲の末尾の次を-1
 for _ in range(M):
-    L, R = map(int, input().split())
-    L = L - 1
-    R = R - 1
-    S_left = S[:L]
-    S_mid = S[L:(R+1)]
-    S_right = S[(R+1):]
-    T_left = T[:L]
-    T_mid = T[L:(R+1)]
-    T_right = T[(R+1):]
-    S = S_left + T_mid + S_right
-    T = T_left + S_mid + T_right
+    l, r = map(int, input().split())
+    cum[l-1] += 1
+    cum[r] -= 1
 
-print(''.join(map(str, S)))
+# いもす法 - Point 2 => 前からじゃんじゃん足していく
+for i in range(N):
+    cum[i + 1] =+ cum[i]
 
-
-
+# もし偶数回スワップ => 元の位置｜奇数回スワップ => t の文字列
+ans = [s[i] if cum[i] % 2 == 0 else t[i] for i in range(N)]
+print(''.join(ans))
