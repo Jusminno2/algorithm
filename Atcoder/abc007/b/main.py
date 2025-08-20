8b090fab966a1dcbafe886d7fdd2942e57abd406
Jusminno2 <g.urakawa@gmail.com>
2025-08-21 07:13:55 +0900

    abc007b

--- Atcoder/abc007/b/main.py
+++ Atcoder/abc007/b/main.py
@@ -1,1 +1,11 @@
+import string
 
+A = input()
+if A == 'a':
+    print(-1)
+else:
+    for a in A:
+        for i in string.ascii_letters:
+            if a > i:
+                print(i)
+                exit()
