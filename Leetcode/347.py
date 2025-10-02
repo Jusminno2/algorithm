40d0e5a762ac80e5c9a21366851b9b7461fd6821
Jusminno2 <g.urakawa@gmail.com>
2025-10-03 07:59:33 +0900

    バケットソートの練習

--- Leetcode/347.py
+++ Leetcode/347.py
@@ -4,14 +4,17 @@ from collections import defaultdict
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        cnt = defaultdict(int)
-        for n in nums:
-            cnt[n] += 1
-        val = list(cnt.values())
-        val.sort(reverse=True)
-        top_k = val[:k]
-        res = list()
-        for l, m in cnt.items():
-            if m in top_k:
-                res.append(l)
-        return res
+        count = {}
+        freq = [[] for _ in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
