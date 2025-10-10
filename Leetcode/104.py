969781bbcf5d0d06fcfda001092b5f360cd1b944
Jusminno2 <g.urakawa@gmail.com>
2025-10-10 09:31:31 +0900

    木の最大深さを求める問題 => BFSによる解法

--- Leetcode/104.py
+++ Leetcode/104.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from collections import deque
 
 
 class TreeNode:
@@ -7,8 +8,21 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+        level = 0
+        q = deque([root])
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
