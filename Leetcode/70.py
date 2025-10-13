6baae0aa6f758ae6b0b4d775aedf08e6bc5d6f12
Jusminno2 <g.urakawa@gmail.com>
2025-10-13 10:10:03 +0900

    メモリを節約できるんですよ

--- Leetcode/70.py
+++ Leetcode/70.py
@@ -1,12 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [0 for _ in range(n + 1)]
-        dp[0] = 1
+        one, two = 1, 1
 
-        for i in range(1, n + 1):
-            if i == 1:
-                dp[i] = 1
-            else:
-                dp[i] = dp[i - 2] + dp[i - 1]
+        for _ in range(n - 1):
+            temp = one
+            one = one + two
+            two = temp
 
-        return dp[n]
+        return one
